a1674dc5b132e033374c1301c71f21bce7593336
piehlm <piehlm@paperstreetenterprises.com>
2021-05-14 10:54:14 -0400

    Lesson 3 - Chapter 11
    Create getZombiesByOwner

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -17,4 +17,10 @@ contract ZombieHelper is ZombieFeeding {
     zombies[_zombieId].dna = _newDna;
   }
 
+  function getZombiesByOwner(address _owner) external view returns(uint[]) {
+    uint[] memory result = new uint[](ownerZombieCount[_owner]);
+
+    return result;
+  }
+
 }
