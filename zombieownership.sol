4bb7355f1239b17b1f3ac479e0d1acc81af767d6
piehlm <piehlm@paperstreetenterprises.com>
2021-05-17 10:02:39 -0400

    lesson 5 - chapter 11
    updated safemath.sol (32 & 16 versions)
    cleaned up zombieownership
    implemented safemath32 & safemath16 within zombiefactory

--- zombieownership.sol
+++ zombieownership.sol
@@ -20,13 +20,13 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
 
   function _transfer(address _from, address _to, uint256 _tokenId) private {
     ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
-    ownerZombieCount[_from] = ownerZombieCount[_from].sub(1);
+    ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].sub(1);
     zombieToOwner[_tokenId] = _to;
     emit Transfer(_from, _to, _tokenId);
   }
 
   function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
-    require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals [_tokenId] == msg.sender);
+    require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
     _transfer(_from, _to, _tokenId);
   }
 
