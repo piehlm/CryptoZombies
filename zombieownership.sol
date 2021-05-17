dad1f6ffcbd6415434f39919003e4355da121e83
piehlm <piehlm@paperstreetenterprises.com>
2021-05-17 10:11:15 -0400

    lesson 5 - chapter 13
    cleanup of zombieownership.

--- zombieownership.sol
+++ zombieownership.sol
@@ -26,13 +26,13 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
   }
 
   function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
-    require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
-    _transfer(_from, _to, _tokenId);
-  }
+      require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+      _transfer(_from, _to, _tokenId);
+    }
 
   function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
-    zombieApprovals[_tokenId] = _approved;
-    emit Approval(msg.sender, _approved, _tokenId);
-  }
+      zombieApprovals[_tokenId] = _approved;
+      emit Approval(msg.sender, _approved, _tokenId);
+    }
 
 }
